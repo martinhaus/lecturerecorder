a22c0a6eeeab4f2c47cb76b3ea4162d8c97d61e6
Martin <hauskrechtmartin@gmail.com>
2018-07-30 17:03:15 +0200

    Remove old files

--- lecture_recorder/src/main/resources/recorder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CAMERA_IP=$1
-AUDIO_SOURCE=$2
-OUTPUT_FILE=$3
-
-COMMAND="arecord -f cd -r 48000 -D "$(AUDIO_SOURCE)" | ffmpeg -thread_queue_size 1024  -i - -thread_queue_size 1024 -i rtsp://"$(CAMERA_IP)" -c:v copy -c:a aac -strict -2 "$(OUTPUT_FILE)""
-eval $(COMMAND)
-exit 0
