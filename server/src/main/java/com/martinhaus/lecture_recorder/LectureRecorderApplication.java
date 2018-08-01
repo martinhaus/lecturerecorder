ef50caac7a95d470493621a39d1b06fd4e88db25
Martin <hauskrechtmartin@gmail.com>
2018-08-01 16:43:05 +0200

    Delete test

--- server/src/main/java/com/martinhaus/lecture_recorder/LectureRecorderApplication.java
+++ server/src/main/java/com/martinhaus/lecture_recorder/LectureRecorderApplication.java
@@ -2,6 +2,7 @@ package com.martinhaus.lecture_recorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,5 +14,10 @@ public class LectureRecorderApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(LectureRecorderApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LectureRecorderApplication.class);
     }
 }
