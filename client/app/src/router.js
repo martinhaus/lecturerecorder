cba183752eae7276b1fd23bd5326ee998ea3453e
Martin <hauskrechtmartin@gmail.com>
2018-08-02 15:35:55 +0200

    deploy

--- client/app/src/router.js
+++ client/app/src/router.js
@@ -7,7 +7,7 @@ import ScheduleRecording from '@/components/ScheduleRecording'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   routes: [
     {
       path: '/',
