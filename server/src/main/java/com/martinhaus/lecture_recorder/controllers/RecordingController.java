e3650ef5b99759b9d83d1b373440bebe6e5fc5e4
Martin <hauskrechtmartin@gmail.com>
2018-08-06 20:32:25 +0200

    New downloader

--- server/src/main/java/com/martinhaus/lecture_recorder/controllers/RecordingController.java
+++ server/src/main/java/com/martinhaus/lecture_recorder/controllers/RecordingController.java
@@ -1,19 +1,18 @@
 package com.martinhaus.lecture_recorder.controllers;
 
+import com.martinhaus.lecture_recorder.common.MultiPartFileSender;
 import com.martinhaus.lecture_recorder.model.Recording;
 import com.martinhaus.lecture_recorder.services.RecordingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -48,19 +47,22 @@ public class RecordingController {
     }
 
     @RequestMapping(path = "/recording/{id}/download", method = RequestMethod.GET)
-    public ResponseEntity<Resource> downloadRecording(@PathVariable("id") long id) throws IOException {
+    public void downloadRecording(@PathVariable("id") long id, HttpServletRequest request, HttpServletResponse response) {
 
         File file = recordingService.getRecordingFile(id);
         HttpHeaders respHeaders = new HttpHeaders();
         respHeaders.setContentType(MediaType.parseMediaType("video/mp4"));
         respHeaders.setContentLength(file.length());
         respHeaders.setContentDispositionFormData("attachment", file.getName());
-
         Path path = Paths.get(file.getAbsolutePath());
-        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
-        return ResponseEntity.ok()
-                .headers(respHeaders)
-                .body(resource);
+        try {
+            MultiPartFileSender.fromPath(path)
+                    .with(request)
+                    .with(response)
+                    .serveResource();
+        } catch (Exception e) {
+
+        }
     }
 }
 
