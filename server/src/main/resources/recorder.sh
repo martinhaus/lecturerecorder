139dd7c4dbb20c5afff8ee814fa3f49969c95c42
martin <hauskrechtmartin@gmail.com>
2018-08-06 10:19:37 +0200

    Edit script

--- server/src/main/resources/recorder.sh
+++ server/src/main/resources/recorder.sh
@@ -5,6 +5,11 @@ AUDIO_SOURCE=$2
 OUTPUT_FILE=$3
 DURATION=$4
 
+echo $CAMERA_IP
+echo $AUDIO_SOURCE
+echo $OUTPUT_FILE
+echo $DURATION
+
 COMMAND="arecord -f cd -r 48000 -D "$AUDIO_SOURCE" | ffmpeg -t $DURATION -thread_queue_size 1024  -i - -thread_queue_size 1024 -i rtsp://"$CAMERA_IP" -c:v copy -c:a aac -strict -2 "$OUTPUT_FILE""
 eval $COMMAND
 exit 0
