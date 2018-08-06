7818c075e22db11558051d8c05fc8dc2dff3d557
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2018-08-06 09:49:53 +0000

    recorder.sh edited online with Bitbucket

--- server/src/main/resources/recorder.sh
+++ server/src/main/resources/recorder.sh
@@ -10,6 +10,7 @@ echo $AUDIO_SOURCE
 echo $OUTPUT_FILE
 echo $DURATION
 
-COMMAND="arecord -f cd -r 48000 -D "$AUDIO_SOURCE" | ffmpeg -t $DURATION -thread_queue_size 1024  -i - -thread_queue_size 1024 -i rtsp://"$CAMERA_IP" -c:v copy -c:a aac -strict -2 "$OUTPUT_FILE""
+COMMAND="arecord -f cd -r 48000 -D "$AUDIO_SOURCE" | ffmpeg -thread_queue_size 1024  -i - -thread_queue_size 1024 -i rtsp://"$CAMERA_IP" -t $DURATION -c:v copy -c:a aac -strict -2 "$OUTPUT_FILE""
+echo $COMMAND
 eval $COMMAND
 exit 0
